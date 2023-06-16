b9b11063c8921d0be472b95083d709b55ecf91a7
SangYoung <tmskan8@gmail.com>
2023-06-16 23:31:23 +0900

    Moment Day1

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-alert("hi")
+let a = 5;
+let b = 2;
+
+let myName = "KSY"
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log('hello ' + myName);
+
+myName = "SangYoung"
+
+console.log('your new name is ' + myName);
+
+const amIFat = false;
+console.log(amIFat);
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// daysOfWeek를 호출 (토요일까지만 있어서 6개출력)
+console.log(daysOfWeek);
+
+// 일요일을 추가  
+daysOfWeek.push("sun")
+
+console.log(daysOfWeek);
+
