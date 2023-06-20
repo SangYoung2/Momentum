c9704b08dda4a281f7338938cfc26c064c661873
SangYoung <tmskan8@gmail.com>
2023-06-21 00:05:20 +0900

    Momentum Day 3

--- app.js
+++ app.js
@@ -44,4 +44,61 @@ function sayHello(){
     console.log('Hello!');
 }
 
-player.sayHello()
+player.sayHello();
+
+// Calculator
+
+const calculator = {
+    plus: function (a, b) {
+        return a + b;
+    },
+
+    minus: function (a, b) {
+        return a - b;
+    },
+
+    times: function (a, b) {
+        return a * b;
+    },
+
+    divice: function (a, b) {
+        return a / b;
+    },
+
+    power: function (a, b) {
+        return a ** b;
+    }
+}
+
+const plusResult = calculator.plus(2,3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesReust = calculator.times(10, minusResult);
+const diviceResult = calculator.divice(timesReust, minusResult);
+const powerResult = calculator.power(diviceResult, minusResult);
+
+// CalculatorKAge
+
+const age = 96;
+function CalculatorKAge(ageOfForeigner) {
+    return ageOfForeigner + 2;
+}
+
+const KrAge = CalculatorKAge(age);
+
+console.log(KrAge);
+
+// if문
+
+const realAge = parseInt(prompt("How old are you"));
+
+if(isNaN(isNaN(realAge) || realAge < 0)){
+    console.log("Please write a number");
+} else if(realAge < 18){
+    console.log("You are too Young");
+}else if(realAge >= 18 && age <= 50){
+    console.log('You can drink');   
+}else if(realAge > 50 && age <= 80){
+    console.log('You should exercise');  
+}else if(age > 80) {
+    console.log("You can do whatever you want.");
+}
